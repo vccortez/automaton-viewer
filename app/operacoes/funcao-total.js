19dff4a5dfe288d7230f680cc9a0a6c40c788bb1
Vitor Cortez <vitor.a.cortez@gmail.com>
2016-05-22 18:10:08 -0300

    Adiciona `funcao-total` e arquivo `index.js`
    Dentro do arquivo `index.js` iremos registrar um nome para display e
    utilizar a função `require` para cada implementação que fizermos.

---
+++ app/operacoes/funcao-total.js
@@ -0,0 +1,42 @@
+function funcaoTotal(automato) {
+  const eventos = new Set();
+
+  console.log(automato);
+
+  for (let i = 0, l = automato.transicoes.length; i < l; ++i) {
+    eventos.add(automato.transicoes[i].evento);
+  }
+
+  let id = automato.estados.length * 2;
+  let nome = `dead${id}`;
+
+  const dead = { id, nome };
+
+  const total = [];
+
+  for (let i = 0, el = automato.estados.length; i < el; ++i) {
+    let estado = automato.estados[i];
+
+    let transicoes = automato.transicoes
+      .filter(t => t.de === estado.id).map(t => t.evento);
+
+    for (let e of eventos) {
+      if (transicoes.indexOf(e) === -1) {
+        total.push({
+          de: estado.id,
+          para: dead.id,
+          evento: e,
+        });
+      }
+    }
+  }
+
+  automato.estados.push(dead);
+  for (let t of total) {
+    automato.transicoes.push(t);
+  }
+
+  return automato;
+}
+
+module.exports = funcaoTotal;
---
+++ app/operacoes/index.js
@@ -0,0 +1,8 @@
+/**
+ * Esse arquivo é responsável por exportar todas
+ * as operações de autômatos encontradas nessa
+ * pasta.
+ */
+module.exports = {
+  'Função Total': require('./funcao-total.js'),
+};
