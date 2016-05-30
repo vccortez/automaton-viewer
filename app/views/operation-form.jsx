d056c95bf2cc317f148b0c92f6b91f2fd2bba75c
Vitor Cortez <vitor.a.cortez@gmail.com>
2016-05-29 21:29:42 -0300

    Remove select de automato

--- app/views/operation-form.jsx
+++ app/views/operation-form.jsx
@@ -2,18 +2,10 @@ const React = require('react');
 const classes = require('classnames');
 
 const OperationForm = React.createClass({
-  getInitialState() {
-    return {
-      operation: null,
-      automaton: null
-    };
-  },
-
   handleSubmit(e) {
     let operation = this.refs.ops.value;
-    let automaton = this.refs.aut.value;
 
-    this.props.action(operation, automaton);
+    this.props.action(operation);
   },
 
   render() {
@@ -28,14 +20,11 @@ const OperationForm = React.createClass({
     return (
       <form className={forms}>
         <fieldset>
-          <legend>Operações em Autômatos</legend>
+          <legend>Operações</legend>
+
           <label for='op-list'>Operação</label>
           <select id='op-list' ref='ops'>{options}</select>
-          <label for='aut-list'>Autômato</label>
-          <select id='aut-list' ref='aut'>
-            <option value='a'>Autômato A</option>
-            <option value='b'>Autômato B</option>
-          </select>
+
           <input type='button' value='Aplicar' className={btns} onClick={this.handleSubmit}/>
         </fieldset>
       </form>
