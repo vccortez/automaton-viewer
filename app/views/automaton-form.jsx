d7562025add9cb1ec59e54ccd87eb28f66514cfa
Vitor Cortez <vitor.a.cortez@gmail.com>
2016-05-29 21:32:55 -0300

    Atualiza ciclo de vida, gerenciamento de estados e transicoes

--- app/views/automaton-form.jsx
+++ app/views/automaton-form.jsx
@@ -1,54 +1,137 @@
 const React = require('react');
+const StateList = require('./state-list.jsx');
+const TransitionList = require('./transition-list.jsx');
 const classes = require('classnames');
 
 const AutomatonForm = React.createClass({
-  getInitialState() {
-    return { form: '{}' };
+  enviarAutomato() {
+    this.props.confirm();
   },
 
-  componentWillReceiveProps(next) {
-    this.setState({
-      form: next.data
-    });
+  atualizarNome() {
+    const json = JSON.parse(this.props.data);
+    json.nome = this.refs.nome.value;
+    this.props.update(JSON.stringify(json));
   },
 
-  onSubmit(event) {
-    const textarea = this.refs.area;
-    let jsonAut;
+  adicionarEstado(e) {
+    const json = JSON.parse(this.props.data);
 
-    try {
-      jsonAut = JSON.parse(textarea.value);
-    } catch (err) {
-      console.error(err);
-      return;
+    let maxId = 0;
+
+    if (json.estados.length > 0) {
+      maxId = json.estados.reduce((p, c) => {
+        return Math.max(p, c.id);
+      }, maxId);
     }
 
-    this.props.submit(jsonAut);
+    const novo = {
+      id: maxId + 1,
+      nome: e.nome
+    };
+
+    json.estados.push(novo);
+
+    if (e.inicial)
+      json.inicial.push(novo.id);
+
+    if (e.final)
+      json.final.push(novo.id);
+
+    this.props.update(JSON.stringify(json));
   },
 
-  handleChange(event) {
-    const json = this.refs.area.value;
+  removerEstado(id) {
+    const json = JSON.parse(this.props.data);
 
-    this.props.change(json);
+    json.estados = json.estados.filter(e => e.id != id);
+    json.transicoes = json.transicoes.filter(t => t.de != id && t.para != id);
+
+    this.props.update(JSON.stringify(json));
+  },
+
+  adicionarTransicao(t) {
+    const json = JSON.parse(this.props.data);
+
+    const nova = {
+      de: t.de,
+      para: t.para,
+      evento: t.evento
+    };
+
+    json.transicoes.push(nova);
+
+    this.props.update(JSON.stringify(json));
+  },
+
+  removerTransicao(id) {
+    const json = JSON.parse(this.props.data);
+
+    json.transicoes = json.transicoes.filter((t, i) => i != id);
+
+    this.props.update(JSON.stringify(json));
   },
 
   render() {
-    const forms = classes('form-area', 'pure-form', 'pure-form-aligned');
+    const automato = JSON.parse(this.props.data);
+
+    const nome = automato.nome;
+
+    const estados = automato.estados.map(e => {
+      return {
+        id: e.id,
+        nome: e.nome,
+        inicial: automato.inicial.includes(e.id),
+        final: automato.final.includes(e.id)
+      };
+    });
+
+    const transicoes = automato.transicoes.map((e, i) => {
+      return {
+        id: i,
+        de: estados.find(s => s.id == e.de),
+        para: estados.find(s => s.id == e.para),
+        evento: e.evento
+      };
+    });
+
+    const form = classes('form-area', 'pure-form', 'pure-form-stacked');
     const btns = classes('pure-button', 'pure-button-primary');
     const group = classes('pure-control-group');
     const control = classes('pure-controls');
 
     return (
-      <form className={forms}>
+      <form className={form}>
         <fieldset>
-          <legend>Informações do Autômato</legend>
+
+          <legend>Autômato</legend>
+
           <section className={group}>
-            <label for='json-area'>JSON</label>
-            <textarea id='json-area' ref='area' value={this.state.form} onChange={this.handleChange}/>
+            <label for='nome'>Nome</label>
+            <input type='text' placeholder='Autômato' id='nome' required autofocus value={nome} onChange={this.atualizarNome} ref='nome'/>
           </section>
+
+          <section className={group}>
+            <label>Estados</label>
+            <StateList
+              data={estados}
+              add={this.adicionarEstado}
+              remove={this.removerEstado}/>
+          </section>
+
+          <section className={group}>
+            <label>Transições</label>
+            <TransitionList
+              data={transicoes}
+              datast={estados}
+              add={this.adicionarTransicao}
+              remove={this.removerTransicao}/>
+          </section>
+
           <section className={control}>
-            <input type='button' value='Atualizar' className={btns} onClick={this.onSubmit} />
+            <input type='button' value='Confirmar' className={btns} onClick={this.enviarAutomato} />
           </section>
+
         </fieldset>
       </form>
     );
